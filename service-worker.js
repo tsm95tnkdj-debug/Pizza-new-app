1adbc3e451879ed35846b8adc4c88fe54a7c121e
tsm95tnkdj-debug <tsm95tnkdj@privaterelay.appleid.com>
2026-06-19 20:45:29 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,4 @@
-const CACHE_NAME = "calculateur-pizza-complet-v1";
+const CACHE_NAME = "pizza-lab-fun-v2";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
-});
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request))); });
